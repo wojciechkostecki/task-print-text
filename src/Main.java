ab5943a37ea93f8bf4b0cca60c771515585bc4dc
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-03-24 17:28:21 +0100

    Program to print the inscription without using quotation marks

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        char x = 0x004B;
+        char y = 0x004F;
+        char z = 0x0054;
+
+        System.out.print(x);
+        System.out.print(y);
+        System.out.print(z);
+    }
+}
